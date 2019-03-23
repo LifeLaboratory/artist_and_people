d32a777d05eea5f5b99e495edd944a2da7d73c1d
admin <admin@illspill.org>
2019-03-24 02:47:50 +0300

    Frontend: fix login

--- frontend/src/app/middleware/sagas.js
+++ frontend/src/app/middleware/sagas.js
@@ -3,42 +3,12 @@ import * as ActionTypes from "../constants/ActionTypes";
 import fetch from "cross-fetch"
 import { push, goBack } from 'connected-react-router'
 
-async function fetchLogin(credentials) {
-    return fetch('http://hackuniversity.crabdance.com/api/v1/auth', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(credentials)
-    })
-}
-
-function* tryLogin(action) {
-
-    try {
-        const response = yield call(fetchLogin, action.credentials);
-
-        switch (response.status) {
-            case 200:
-                yield put({type: ActionTypes.LOGIN_SUCCEEDED})
-                break
-            case 401:
-                yield put({type: ActionTypes.LOGIN_FAILED, message: "Неправильный логин или пароль"})
-                break
-            default:
-                console.error('Unknown status from server: ' + response.status)
-                yield put({type: ActionTypes.LOGIN_FAILED, message: "Неизвестная ошибка. Попробуйте позже"})
-        }
-    }
-    catch (e) {
-        console.error(e)
-        yield put({type: ActionTypes.LOGIN_FAILED, message: "Неизвестная ошибка. Попробуйте позже"})
-    }
-
-}
-
 function* tryOpenFire() {
     yield put(push('/fire'))
+}
+
+function* tryOpenEvents() {
+    yield put(push('/events'))
 }
 
 function* tryGoBack() {
@@ -47,7 +17,7 @@ function* tryGoBack() {
 
 
 export default function* mySaga() {
-    yield takeEvery(ActionTypes.TRY_LOGIN, tryLogin);
+    yield takeEvery(ActionTypes.TRY_LOGIN, tryOpenEvents);
     yield takeEvery(ActionTypes.OPEN_FIRE, tryOpenFire);
     yield takeEvery(ActionTypes.GO_BACK, tryGoBack);
 }
