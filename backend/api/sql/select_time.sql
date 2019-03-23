789e59ec55d369d16d66ff3815d7cbb623814e5d
raldenprog <raldenprog@mail.ru>
2019-03-23 10:51:36 +0300

    time

--- backend/api/sql/select_time.sql
+++ backend/api/sql/select_time.sql
@@ -1,6 +1,6 @@
 select
-       case when start > now() then "end"
-            else start
+       case when start > now() then "end" - now()
+            else start - now()
         end::text as "time"
 from times
 where id_concert = '{id_concert}'
